da1ee5d894f469b36767929703e06a2d3ba05463
daniele_leao <dani.leao89@gmail.com>
2019-09-03 21:48:25 -0300

    Frontend with reactjs

---
+++ src/pages/Login.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import './Login.css';
+import logo from '../assets/logo.svg';
+
+import api from '../services/api';
+
+
+export default function Login({history}) {
+   const [username, setUsername] = useState('');
+
+   async function handleSubmit(e) {
+      e.preventDefault();
+
+      const response = await api.post('/devs', {
+         username
+      });
+
+      const { _id }  = response.data;
+
+      console.log(response);
+
+      history.push(`/dev/${_id}`);
+   }
+
+
+    return (
+       <div className="login-container">
+         <form onSubmit={handleSubmit}>
+         <img src={logo} alt="TinDev"/>
+         <input type="text"
+            placeholder="Digite seu usuário no Github"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+         />
+         <button type="submit">
+            Enviar
+         </button>
+         </form>
+       </div>
+
+    );
+}
